233a1461e1c34af25142be2058d483b0ed4e0e57
tomerst3 <tomerst3@gmail.com>
2019-09-16 13:42:32 +0300

    Updates

---
+++ precache-manifest.b0ee17cf4c4401cf3a1cf4a09b71029c.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "384e18b359ab39013c4d89a1670c0c89",
+    "url": "/Mini_Facebook/index.html"
+  },
+  {
+    "revision": "7428941553d32554d5e7",
+    "url": "/Mini_Facebook/static/css/2.22a7d4ef.chunk.css"
+  },
+  {
+    "revision": "a3bdfcd10c36dd9e6dd7",
+    "url": "/Mini_Facebook/static/css/main.5d2734dd.chunk.css"
+  },
+  {
+    "revision": "7428941553d32554d5e7",
+    "url": "/Mini_Facebook/static/js/2.45eea058.chunk.js"
+  },
+  {
+    "revision": "a3bdfcd10c36dd9e6dd7",
+    "url": "/Mini_Facebook/static/js/main.8de85dd1.chunk.js"
+  },
+  {
+    "revision": "fbb5569e8f6396bb5436",
+    "url": "/Mini_Facebook/static/js/runtime~main.32c41f51.js"
+  },
+  {
+    "revision": "d809403cc6f40235bc0ed76ab973c778",
+    "url": "/Mini_Facebook/static/media/bf.d809403c.jpeg"
+  },
+  {
+    "revision": "e6b2e5db327df5bbeab1db557642766b",
+    "url": "/Mini_Facebook/static/media/bf2.e6b2e5db.jpeg"
+  },
+  {
+    "revision": "2165957ca066f0ccea64d607ab082f71",
+    "url": "/Mini_Facebook/static/media/cod.2165957c.jpg"
+  },
+  {
+    "revision": "2d464028a927679b44d2867489bf66db",
+    "url": "/Mini_Facebook/static/media/gots.2d464028.jpg"
+  },
+  {
+    "revision": "b161a4df7fff32d3a10628341c17ad8a",
+    "url": "/Mini_Facebook/static/media/me.b161a4df.jpg"
+  },
+  {
+    "revision": "ce339cab9222188a6a7c1e041d7bcaa7",
+    "url": "/Mini_Facebook/static/media/swtor.ce339cab.jpg"
+  }
+]);
